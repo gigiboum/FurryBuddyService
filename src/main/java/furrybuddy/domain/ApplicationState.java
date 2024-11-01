57b6a21d79651c6d583b49338d108b6765fe54f6
audreymartelly <73237246+audreymartelly@users.noreply.github.com>
2024-11-01 17:47:18 +0100

    set up server deployment

--- src/main/java/furrybuddy/domain/ApplicationState.java
+++ src/main/java/furrybuddy/domain/ApplicationState.java
@@ -1,4 +1,49 @@
 package furrybuddy.domain;
 
+import Domain.Adopter;
+import Domain.Pet;
+import Domain.PetOwner;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@ApplicationScoped
 public class ApplicationState {
+
+    private Map<UUID, Adopter> adopters;
+    private Map<UUID, PetOwner> petOwners;
+    private Map<UUID, Pet> pets;
+
+    @PostConstruct
+    public void init() {
+        adopters = new HashMap<>();
+        petOwners = new HashMap<>();
+        pets = new HashMap<>();
+        populateApplicationState();
+    }
+
+    private void populateApplicationState() {
+
+        //Utils.testModeOn()
+
+//        var Pepper =
+    }
+
+    public Pet addPet(Pet pet) {
+        if (pet.getPetID() != null) {
+            return addPet(pet.getPetID(), pet);
+        }
+        return addPet(UUID.randomUUID(), pet);
+    }
+
+    public Pet addPet(UUID petID, Pet pet) {
+        pet.setPetID(petID);
+        pets.put(petID, pet);
+        return pet;
+    }
+
+
 }
